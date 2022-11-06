e4226a28d2f9d04d0792ad31807b33b074b69e3f
ThisGuyCodes1106 <79881489+ThisGuyCodes1106@users.noreply.github.com>
2022-11-06 14:45:10 +0000

    deploy 1.01

---
+++ index.js
@@ -0,0 +1,45 @@
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const pushButton = document.getElementById("push-btn")
+const unshiftButton = document.getElementById("unshift-btn")
+const shiftButton = document.getElementById("shift-btn")
+const popButton = document.getElementById("pop-btn")
+const emojiInput = document.getElementById("emoji-input")
+
+const emojiContainer = document.getElementById("emojiContainer")
+
+function renderEmojis() {
+  emojiContainer.innerHTML = ""
+  for (let i = 0; i < myEmojis.length; i++) {
+      emojiContainer.innerHTML += ("<span>" + myEmojis[i] + "</span>")
+  }
+}
+
+renderEmojis()
+
+unshiftButton.addEventListener("click", function() {
+  if (emojiInput.value) {
+    myEmojis.unshift(emojiInput.value)
+    emojiInput.value = ""
+    renderEmojis()
+  }
+})
+
+pushButton.addEventListener("click", function() {
+  if (emojiInput.value) {
+    myEmojis.push(emojiInput.value)
+    emojiInput.value = ""
+    renderEmojis()
+  }
+})
+
+shiftButton.addEventListener("click", function() {
+    myEmojis.shift()
+    emojiInput.value = ""
+    renderEmojis()
+})
+
+popButton.addEventListener("click", function() {
+    myEmojis.pop()
+    emojiInput.value = ""
+    renderEmojis()
+})
